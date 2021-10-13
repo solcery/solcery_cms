f6c8f37caacc6e5630e87a35a5417d29121d28e3
teuzet <teuzet92@gmail.com>
2021-10-13 20:11:54 +0200

    SolceryMenu

--- src/components/SolceryMenu/index.tsx
+++ src/components/SolceryMenu/index.tsx
@@ -2,40 +2,40 @@ import React, { useState, useEffect } from "react";
 import { useConnection } from "../../contexts/connection";
 import { useProject } from "../../contexts/project"
 import { SystemProgram } from "@solana/web3.js";
-import "./style.less";
 import { TemplateData, Storage} from "../../solcery/classes"
+import { Menu} from "antd"
+
+const { SubMenu } = Menu;
 
 export const SolceryMenu = () => {
   const { project } = useProject();
   const connection = useConnection();
   var [ templates, setTemplates ] = useState<TemplateData[]>([]);  
-  var [ loaded, setLoaded ] = useState<boolean>(false);
 
   useEffect(() => { 
-    if (!loaded) {
-      if (project) {
-        (async () => {
-          const strg = await Storage.get(connection, project?.templateStorage)
-          setTemplates(await TemplateData.getAll(connection, strg.accounts))
-        })()
-        setLoaded(true)
-      }
-    }
-  });
+    if (project)
+      (async () => {
+        const strg = await Storage.get(connection, project?.templateStorage)
+        setTemplates(await TemplateData.getAll(connection, strg.accounts))
+      })()
+  }, [ project ]);
 
-  if (templates.length > 0) 
-    return (
-      <div className='Solcery-Bar'>
-        <a key='play' style={{padding: '25px 10px'}} href='/#/play'>PLAY</a>
-        <a key='home' style={{padding: '25px 10px'}} href='/#/'>Home</a>
-        { templates.map((tpl) => {
-          return (<a style={{padding: '25px 10px'}} key={tpl.publicKey.toBase58()} href={'/#/template/' + tpl.publicKey.toBase58()}>{tpl.name}</a>)
-        })}
-        
-        
-      </div>
-    );
+  const handleClick = (event: any) => {
+
+  }
+
   return (
-    <div>NONE</div>
-  )
+    <Menu onClick={handleClick} mode="horizontal">
+      <Menu.Item key="play">
+        <a href="/#/play">PLAY</a>
+      </Menu.Item>
+      <Menu.Item key="home">
+        <a key='home' href='/#/'>Home</a>
+      </Menu.Item>
+      {templates.map((tpl) => 
+        <Menu.Item key={tpl.publicKey.toBase58()}>
+          <a href={'/#/template/' + tpl.publicKey.toBase58()}>{tpl.name}</a>
+        </Menu.Item>)
+      }
+    </Menu>);
 };
