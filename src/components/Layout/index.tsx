7534bb8499c6a1e2a4f1a192e99b1ed095be2ad9
teuzet <teuzet92@gmail.com>
2021-10-13 20:11:29 +0200

    updated layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,18 +11,7 @@ const { Header, Content } = Layout;
 export const AppLayout = React.memo((props: any) => {
   return (
     <div className="App wormhole-bg">
-      <Layout>
-        <Header className="App-Bar">
-
-          <Link to="/">
-            <div className="app-title">
-              <h2>SOLCERY</h2>
-            </div>
-          </Link>
-          <AppBar />
-        </Header>
-        <Content style={{ padding: "0 0px" }}>{props.children}</Content>
-      </Layout>
+      {props.children}
     </div>
   );
 });
