16604dc39c2f208e98fbb82347eb2cd32be1a628
tjumma <tim@jumma.pro>
2021-11-07 00:28:24 +0000

    game_31

--- src/views/play/index.tsx
+++ src/views/play/index.tsx
@@ -39,10 +39,10 @@ export const GameObjectView = (props: {
 }
 
 const unityPlayContext = new UnityContext({
-  loaderUrl: "game/game_30.loader.js",
-  dataUrl: "game/game_30.data",
-  frameworkUrl: "game/game_30.framework.js",
-  codeUrl: "game/game_30.wasm",
+  loaderUrl: "game/game_31.loader.js",
+  dataUrl: "game/game_31.data",
+  frameworkUrl: "game/game_31.framework.js",
+  codeUrl: "game/game_31.wasm",
 })
 
 export const PlayView = () => {
