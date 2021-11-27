27a9bf0bbbb1dd5810e79687d10472a7d28ff563
tjumma <tjumma@solcery.xyz>
2021-11-27 16:43:34 +0300

    game_35

--- src/views/play/index.tsx
+++ src/views/play/index.tsx
@@ -39,10 +39,10 @@ export const GameObjectView = (props: {
 }
 
 const unityPlayContext = new UnityContext({
-  loaderUrl: "game/game_33.loader.js",
-  dataUrl: "game/game_33.data",
-  frameworkUrl: "game/game_33.framework.js",
-  codeUrl: "game/game_33.wasm",
+  loaderUrl: "game/game_35.loader.js",
+  dataUrl: "game/game_35.data",
+  frameworkUrl: "game/game_35.framework.js",
+  codeUrl: "game/game_35.wasm",
 })
 
 export const PlayView = () => {
