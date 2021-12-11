446f10c90c18c09030e7b4d2218278e279274f0c
tjumma <tjumma@solcery.xyz>
2021-12-11 16:31:24 +0300

    node_editor_16

--- src/solcery/types/brick/components.tsx
+++ src/solcery/types/brick/components.tsx
@@ -8,10 +8,10 @@ import { SInt, SString, ValueRenderParams, SBrick } from '../index'
 
 
 const unityContext = new UnityContext({
-  loaderUrl: "node_editor/node_editor_15.loader.js",
-  dataUrl: "node_editor/node_editor_15.data",
-  frameworkUrl: "node_editor/node_editor_15.framework.js",
-  codeUrl: "node_editor/node_editor_15.wasm",
+  loaderUrl: "node_editor/node_editor_16.loader.js",
+  dataUrl: "node_editor/node_editor_16.data",
+  frameworkUrl: "node_editor/node_editor_16.framework.js",
+  codeUrl: "node_editor/node_editor_16.wasm",
 })
 
 export const ValueRender = (props: ValueRenderParams) => {
