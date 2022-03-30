1a81ec308a92d819ee25968bc786a04282cc7d16
teuzet <teuzet92@gmail.com>
2022-03-30 03:20:54 +0200

    Speeded up storage loading, now it is asynchronous

--- src/content/loadable/storage.tsx
+++ src/content/loadable/storage.tsx
@@ -2,7 +2,9 @@ import { deserializeUnchecked } from 'borsh';
 import { PublicKey, Connection } from "@solana/web3.js";
 import { schema, StorageData } from './schema'
 
-let Master: any = {}
+let Master: any = {
+
+}
 
 Master.fromBinary = function(data: any) {
   let src = deserializeUnchecked(schema, StorageData, data)
@@ -22,12 +24,14 @@ Master.loadAll = async function(connection: Connection) { //TODO Promise
   let toLoad = objects.filter((obj: any) => !obj.isLoaded);
   let pubkeys = toLoad.map((obj: any) => new PublicKey(obj.id))
   let accInfos = await connection.getMultipleAccountsInfo(pubkeys)
+  let items: any[] = [];
   for (let i = 0; i < accInfos.length; i++) {
     let accInfo = accInfos[i]
     if (accInfo !== null) {
-      await toLoad[i].load(connection, accInfo.data.slice(33))
+      items.push(toLoad[i].load(connection, accInfo.data.slice(33)))
     }
   }
+  await Promise.all(items)
 }
 
 Master.onSolanaAccountChanged = function (connection: Connection, data: any) {
